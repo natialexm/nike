b7f748794380ec187153403a5f098191fce02c14
natialexm <natnael.alex.m@gmail.com>
2024-09-03 14:44:17 +0300

    Complete basic navigation and additional setup

---
+++ src/sections/index.js
@@ -0,0 +1,22 @@
+import Hero from './Hero';
+import CustomerReview from './CustomerReview';
+import Footer from './Footer';
+import Services from './Services';
+import PopularProducts from './PopularProducts';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+
+
+
+export {
+    Hero,
+    CustomerReview,
+    Footer,
+    Services,
+    PopularProducts,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+};
