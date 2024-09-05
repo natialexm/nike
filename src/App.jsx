4a18a37c6d25bd04305424e80e4474a0bef00d59
natialexm <natnael.alex.m@gmail.com>
2024-09-05 08:03:14 +0300

    Add and complete Hero section with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import { CustomerReview, Footer, Hero, PopularProducts, Services, SpecialOffer, SuperQuality, Subscribe } from "./sections";
+import {
+  CustomerReview,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+  Subscribe,
+} from "./sections";
 import Nav from "./components/Nav";
 
 const App = () => (
-  
   <main className="relative">
     <Nav />
-    
-    <section className="xl:padding-1 wide:padding-r padding-b">
+
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
 
     <section className="padding">
-        <PopularProducts />
+      <PopularProducts />
     </section>
 
     <section className="padding">
-     <SuperQuality />
+      <SuperQuality />
     </section>
 
     <section className="padding-x py-10">
-     <Services />
+      <Services />
     </section>
 
     <section className="padding">
