39f466d43554bfb44f037dcb5a6f6cae7baf420c
natialexm <natnael.alex.m@gmail.com>
2024-09-05 22:53:59 +0300

    Finished the entire project. Added sections for Footer, Popular Products, Subscribe, Special Offers, Superior Quality, and Services.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,30 @@
-const Button = ({lable, iconURL}) => {
+const Button = ({
+  lable,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
-        {lable}
-        <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"/>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"} `}
+    >
+      {lable}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,13 @@
+import { ServiceCard } from "../components";
+import { services } from "../constants";
 const Services = () => {
   return (
-    <div>
-      Services
-    </div>
-  )
-}
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
 
-export default Services
+export default Services;
